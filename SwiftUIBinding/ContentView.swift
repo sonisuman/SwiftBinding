030f36f28d83b8ab81ddd69ea9f3eb93416afb35
soni suman <Sonisuman67@gmail.com>
2019-12-10 14:30:55 +0530

    Initial Commit

---
+++ SwiftUIBinding/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIBinding
+//
+//  Created by Soni Suman on 10/12/19.
+//  Copyright © 2019 Soni Suman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
